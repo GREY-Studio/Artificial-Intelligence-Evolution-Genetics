93dbfbbdc5627351458bcb97d9c82aea90a78226
GREY-Studio <legofgoat@gmail.com>
2017-07-17 01:16:22 +0200

    Basic Behavior & DNA

--- sketch.js
+++ sketch.js
@@ -6,34 +6,73 @@
 
 
 // Variables
-var $vehicle,
-    $food = [];
+var $vehicles = [],
+    $food = [],
+    $poison = [];
 
 // Setup function
 function setup() {
 
   createCanvas((window.innerWidth - 60), (window.innerHeight - 105));
+  for (var i = 0; i < 10; i++) {
+    var x = random(width),
+        y = random(height);
 
+    $vehicles[i] = new Vehicle(x, y);
+  }
 
-  $vehicle = new Vehicle(width/2, height/2);
+  // Generate food arrays
+  for (var i = 0; i < 10; i++) {
 
+    var x = random(width),
+        y = random(height);
+
+    $food.push(createVector(x, y));
+  }
+
+  for (var i = 0; i < 10; i++) {
+
+    var x = random(width),
+        y = random(height);
+
+    $poison.push(createVector(x, y));
+  }
 
 }
 
 // Draw function
 function draw() {
 
-  var $target = createVector(mouseX, mouseY);
+  // Draw target
+
+  //var $target = createVector(mouseX, mouseY);
 
   background(51);
   fill(127);
   stroke(200);
   strokeWeight(2);
 
-  ellipse($target.x, $target.y, 48, 48);
+  //ellipse($target.x, $target.y, 48, 48);
 
-  $vehicle.seek($target);
-  $vehicle.update();
-  $vehicle.display();
+
+  // Draw (good) food
+  for(var i = 0; i < $food.length; i++) {
+    fill(152, 191, 110);
+    noStroke();
+    ellipse($food[i].x, $food[i].y, 8, 8);
+  }
+
+  // Draw (bad) food
+  for(var i = 0; i < $poison.length; i++) {
+    fill(255, 84, 70);
+    noStroke();
+    ellipse($poison[i].x, $poison[i].y, 8, 8);
+  }
+
+  for (var i = 0; i < $vehicles.length; i++) {
+    $vehicles[i].behaviors($food, $poison);
+    $vehicles[i].update();
+    $vehicles[i].display();
+  }
 
 }
--- vehicle.js
+++ vehicle.js
@@ -12,8 +12,16 @@ function Vehicle(x, y) {
   this.velocity = createVector(0, -2);
   this.position = createVector(x, y);
   this.r = 6;
-  this.maxspeed = 8;
-  this.maxforce = 0.2;
+  this.maxspeed = 5;
+  this.maxforce = 0.5;
+
+  // Health
+  this.health = 1;
+
+  // DNA
+  this.dna = [];
+  this.dna[0] = random(-5, 5);
+  this.dna[1] = random(-5, 5);
 
   // Method to update location
   this.update = function() {
@@ -38,6 +46,47 @@ function Vehicle(x, y) {
 
   }
 
+  // Method declaring behaviors
+  this.behaviors = function(good, bad) {
+
+    var $steerG = this.eat(good),
+        $steerB = this.eat(bad);
+
+    $steerG.mult(this.dna[0]);
+    $steerB.mult(this.dna[1]);
+
+    this.applyForce($steerG);
+    this.applyForce($steerB);
+
+  }
+
+  // Method that controll eating
+  this.eat = function(list) {
+
+    var $record = Infinity,
+        $closest = -1;
+
+    for(var i = 0; i < list.length; i++) {
+
+      var $distance = this.position.dist(list[i]);
+
+      if($distance < $record) {
+        $record = $distance;
+        $closest = i;
+      }
+
+    }
+
+    if($record < 5) {
+      list.splice($closest, 1);
+    } else if($closest > -1) {
+      return this.seek(list[$closest]);
+    }
+
+    return createVector(0, 0);
+
+  }
+
   // Method that calculates a steering force
   // 'Steer' = 'Decired minus velocity'
   this.seek = function(target) {
@@ -53,7 +102,9 @@ function Vehicle(x, y) {
     // Limit to maxspeed
     $steer.limit(this.maxforce);
 
-    this.applyForce($steer);
+    //this.applyForce($steer);
+
+    return $steer;
 
   }
 
@@ -61,16 +112,22 @@ function Vehicle(x, y) {
   this.display = function() {
 
     // Draw a triangle rotated in the direction of velocity
-    var $theta = this.velocity.heading() + PI / 2;
+    var $angle = this.velocity.heading() + PI / 2;
 
     push();
 
     translate(this.position.x, this.position.y);
-    rotate($theta);
+    rotate($angle);
+
+    stroke(152, 191, 110);
+    line(0, 0, 0, -this.dna[0] * 20);
+    stroke(255, 84, 70);
+    line(0, 0, 0, -this.dna[1] * 20);
 
     // Geometry
     fill(127);
     stroke(200);
+    strokeWeight(2);
 
     beginShape();
     vertex(0, -this.r * 2);
